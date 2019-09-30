17b1a94c9480708e5b110598ef91817b020a4da8
DrDany <ddv_2010@mail.ru>
2019-09-30 15:52:43 +0300

    '123'

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,27 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    ''' тест нового поситетеля'''
+
+    def setUp(self):
+        '''Установка'''
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        '''Демонтаж'''
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        '''тест: можно начать список и получить его позже11'''
+        self.browser.get('http://localhost:8000')
+
+        # Она видит, что заголовок и шапка страницы говорят о
+        # списках неотложных дел
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Закончить тест!')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Создайте здесь представления свои.
+def home_page():
+    pass
+'''домашняя страница'''
