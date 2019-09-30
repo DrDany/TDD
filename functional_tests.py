84613c09188ac51d5444039866c91813e80b04f2
DrDany <ddv_2010@mail.ru>
2019-09-30 17:32:14 +0300

    dd all

--- functional_tests.py
+++ functional_tests.py
@@ -23,7 +23,7 @@ class NewVisitorTest(unittest.TestCase):
         # списках неотложных дел
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('hl').text
-        self.assertln('To-Do', header_text)
+        self.assertIn('To-Do', header_text)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
@@ -37,7 +37,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Купить павлиньи перья' for row in rows)
+            any(row.text == '1: Купить павлиньи перья' for row in rows),
+            "Новый элемент списка не появился в таблице"
         )
 
         self.fail('Закончить тест!')
--- lists/views.py
+++ lists/views.py
@@ -4,5 +4,6 @@ from django.http import HttpResponse
 
 # Создайте здесь представления свои.
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'home.html')
+
 '''домашняя страница'''
