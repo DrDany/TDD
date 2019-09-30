edf5d02f988a2d357fe7f9aef8ad119bf5376133
DrDany <ddv_2010@mail.ru>
2019-09-30 16:07:10 +0300

    add new

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
 
 # Создайте здесь представления свои.
-def home_page():
-    pass
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
 '''домашняя страница'''
